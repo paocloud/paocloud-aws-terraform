0662480a3cf8d00d6e9c14d87a6a5cbede79d042
Payungsak Klinchampa <payungsakpk@payungsakpk.xyz>
2021-04-28 23:28:03 +0700

    Create user_data.sh

---
+++ eks/user_data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+################# AWS EKS USER DATA CONFIG ################# 
+
+#### Update OS ####
+sudo yum -y update
+
+#### Timezone Config ####
+sudo timedatectl set-timezone Asia/Bangkok
+
+#### Auto Cleanup Docker Container Images ####
+#### Ref : https://aws.amazon.com/premiumsupport/knowledge-center/eks-worker-nodes-image-cache/ ####
+
+set -o xtrace
+
+# Inject imageGCHighThresholdPercent value unless it has already been set.
+if ! grep -q imageGCHighThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json; 
+then 
+    sed -i '/"apiVersion*/a \ \ "imageGCHighThresholdPercent": 70,' /etc/kubernetes/kubelet/kubelet-config.json
+fi
+
+# Inject imageGCLowThresholdPercent value unless it has already been set.
+if ! grep -q imageGCLowThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json; 
+then 
+    sed -i '/"imageGCHigh*/a \ \ "imageGCLowThresholdPercent": 50,' /etc/kubernetes/kubelet/kubelet-config.json
+fi
+
+/etc/eks/bootstrap.sh wowza-eks
+
+#### Hostname Config ####
+sudo hostnamectl set-hostname paocloud-eks-node
